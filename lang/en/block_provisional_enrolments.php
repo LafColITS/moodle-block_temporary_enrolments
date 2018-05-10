34bceb0e8da25943e8c809a07097536c23d81cc6
Andrew Zito <zitoa@lafayette.edu>
2018-05-10 14:11:44 -0400

    Added lang files

---
+++ lang/en/block_provisional_enrolments.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Version details.
+ *
+ * @package    block_temporary_enrolments
+ * @copyright  2018 onwards Lafayette College ITS
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['admin_student_message_desc'] = 'Student display message';
+$string['admin_student_message_subdesc'] = 'What temporarily enrolled students see in the block';
+$string['admin_student_message_override_desc'] = 'Override student display message?';
+$string['admin_student_message_override_subdesc'] = 'Check this to override instance level student display message setting. This will alter any current instance level settings, and prevent further changes to the setting at the instance level.';
+$string['admin_urgent_threshold_desc'] = 'Urgent threshold default';
+$string['admin_urgent_threshold_subdesc'] = 'The point at which the student display message becomes a warning';
+$string['admin_urgent_threshold_override_desc'] = 'Override urgent threshold?';
+$string['admin_urgent_threshold_override_subdesc'] = 'Check this to override instance level urgent threshold setting. This will alter any current instance level settings, and prevent further changes to the setting at the instance level.';
+$string['pluginname'] = 'Provisional enrolments';
+$string['temporary_enrolments:canconfig'] = 'Configure provisional enrolments block';
+$string['temporary_enrolments:canviewall'] = 'View the admin table of temporarily enrolled students for a course';
+$string['temporary_enrolments:addinstance'] = 'Add an instance of the provisional enrolments block';
+$string['student_message'] = 'Student display message';
+$string['student_message_default'] = 'Time remaining in temporary enrollment: {TIMELEFT}';
+$string['title'] = 'Temporary enrollment';
+$string['urgent_threshold'] = 'Urgent threshold';
+$string['warning_instance_level'] = 'Provisional enrolments plugin not enabled, it is required for this block to function.';
+$string['warning_admin_level'] = 'WARNING! Provisional enrolments plugin is not enabled. Please enable it for the proper functioning of this block.';
