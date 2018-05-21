171888c7166bb567d44148d8f9b0395bd05bd547
Andrew Zito <zitoa@lafayette.edu>
2018-05-21 13:22:33 -0400

    Splitting off Moodle 3.4 version

--- version.php
+++ version.php
@@ -24,11 +24,11 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2018052000;
+$plugin->version = 2018052034;
 $plugin->requires = 2017111300;
 $plugin->component = 'block_temporary_enrolments';
 $plugin->maturity = MATURITY_BETA;
 $plugin->release = '1.0.0';
 $plugin->dependencies = array(
-  'local_temporary_enrolments' => 2017060800 // 1.0.0
+  'local_temporary_enrolments' => 2018032700 // 3.4.0
 );
